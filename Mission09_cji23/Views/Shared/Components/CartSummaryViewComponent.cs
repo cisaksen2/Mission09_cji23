118d5c8d794ff382c6b42ff82d0d2d6590486668
cisaksen2 <cji23@byu.edu>
2023-03-14 18:02:10 -0600

    added cart functiontality

---
+++ Mission09_cji23/Views/Shared/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_cji23.Models;
+namespace Mission09_cji23.Components
+{
+    public class PurchaseSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public PurchaseSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
